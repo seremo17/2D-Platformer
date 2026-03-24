c627ee0f6692cef3858cbdda209d02578b2e1e9e
seremo17 <seremo@s.sfusd.edu>
2026-03-24 13:52:18 -0700

    Video 15 Completed

---
+++ Scripts/coin.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+var rotate_speed : float = 3.0
+var bob_height : float = 5.0
+var bob_speed : float = 5.0
+
+@onready var start_pos : Vector2 = global_position
+
+@onready var sprite : Sprite2D = $Sprite
+
+
+func _physics_process(delta):
+	var time = Time.get_unix_time_from_system()
+
+	sprite.scale.x = sin(time * rotate_speed)
+
+	var y_pos = ((1 + sin(time * bob_speed)) / 2) * bob_height
+	global_position.y = start_pos.y - y_pos
