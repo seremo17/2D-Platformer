fe0e2c9595f798b7bbc4db15808ab67d14be31b0
seremo17 <seremo@s.sfusd.edu>
2026-03-24 13:26:15 -0700

    Video 13 Completed

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -8,6 +8,14 @@ extends Area2D
 @onready var start_pos : Vector2 = global_position
 @onready var target_pos : Vector2 = global_position + move_direction
 
+func _ready ():
+	$AnimationPlayer.play("fly")
+	
+	
+
+
+
+
 
 func _physics_process(delta):
 	global_position = global_position.move_toward(target_pos, move_speed * delta)
@@ -18,6 +26,19 @@ func _physics_process(delta):
 			target_pos = start_pos + move_direction
 		else:
 			target_pos = start_pos
+	
+	
+	
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if not body.is_in_group("Player"):
+		return
+	
+	
+	print("deal damage to player")
+	
+	
 	
 	
 	
