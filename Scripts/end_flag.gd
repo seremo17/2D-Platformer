a765b6fe8d38a7466305b1a408c981d26929430a
seremo17 <seremo@s.sfusd.edu>
2026-04-06 12:36:15 -0700

    Completed Checkpoint 3 ( made enemy, made coin, made player stats, made end flag that teleports you to next scene.

--- Scripts/end_flag.gd
+++ Scripts/end_flag.gd
@@ -1,23 +1,14 @@
 extends Area2D
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+@export var scene_to_load : PackedScene
 
 func _on_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	if not body.is_in_group("Player"):
+		return
+	
+	get_tree().change_scene_to_packed(scene_to_load)
+	
+	
+	
+	
+	
