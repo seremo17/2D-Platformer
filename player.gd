6c5960f9ded45191f170416699854496e54caddf
seremo17 <seremo@s.sfusd.edu>
2026-05-28 02:22:19 -0700

    Added hurt and death sound

--- player.gd
+++ player.gd
@@ -17,8 +17,11 @@ var move_input : float
 @onready var anim : AnimationPlayer = $AnimationPlayer
 @onready var audio : AudioStreamPlayer = $AudioStreamPlayer
 
-var take_damage_sfx : AudioStream = preload("res://Audio/take_damage.wav")
-var coin_sfx : AudioStream = preload("res://Audio/coin.wav")
+
+var hurt_sound_end := -1.0
+var take_damage_sfx : AudioStream = preload("res://Audio/Hurt_sound_effect.mp3")
+var coin_sfx : AudioStream = preload("res://Audio/Coin_Collect.mp3")
+
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -41,8 +44,18 @@ func _physics_process(delta):
 	move_and_slide()
 	
 func _process(delta):
-	if velocity.x != 0:
-		sprite.flip_h = velocity.x > 0
+	
+	if Input.is_action_pressed("move_left"):
+		sprite.flip_h = true
+	if Input.is_action_pressed("move_right"):
+		sprite.flip_h = false
+	
+	
+	if hurt_sound_end > 0 and audio.get_playback_position() >= hurt_sound_end:
+		audio.stop()
+		hurt_sound_end = -1.0
+	
+	
 	
 	if global_position.y >200:
 		game_over()
@@ -63,10 +76,13 @@ func take_damage(amount : int):
 	health -= amount
 	OnUpdateHealth.emit(health)
 	_damage_flash()
-	play_sound(take_damage_sfx)
+	
 	
 	if health <= 0:
-		call_deferred("game_over")
+		await dead_sound_cut()
+		game_over()
+	else:
+		take_damage_sound_cut(1.77, 2.08)
 	
 	
 func game_over ():
@@ -86,3 +102,23 @@ func _damage_flash ():
 func play_sound (sound : AudioStream):
 	audio.stream = sound
 	audio.play()
+	
+	
+
+func take_damage_sound_cut(start_time: float, end_time: float):
+	audio.stream = take_damage_sfx
+	audio.play(1.77)
+	hurt_sound_end = 2.08
+
+
+
+func dead_sound_cut():
+	var start := 3.05
+	var end := 3.95
+	var duration := 0.2
+	
+	audio.stream = take_damage_sfx
+	audio.play(3.05)
+	
+	await get_tree().create_timer(duration).timeout
+	audio.stop()
